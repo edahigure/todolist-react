d7d3ed8a1edbae86fb0afb6d99e758903da46f98
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-02-20 14:31:15 -0700

    Build basic intro

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+
+
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h1> Todos</h1>
+        <h2> items will persist in the local browser storage  </h2>
+        <ul>
+            inputs items here
+            <li> Set up develop enviroment</li>
+            <li>Develop website and add content</li>
+            <li>Deploy to live server</li>
+        </ul>
     </div>
   )
 }
